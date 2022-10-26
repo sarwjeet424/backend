88ff4ad37231fba2511fc63da877d119644a0422
sarwjeet424 <sarwjeet424@gmail.com>
2022-10-26 18:13:58 +0530

    Mongodb session 2 (book)

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,16 +4,40 @@ const router = express.Router();
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
 
+//.........................Assignment Date 25-10-2022............................//
+
+// 1.
 router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/getBooksData", BookController.getBooksData )
+
+// 2.
+
+router.get("/getBookList", BookController.getBooksList )
+
+// 3.
+
+router.post("/getBooksInYear",BookController.getBooksInYear )
+
+// 4.
+
+router.get("/getParticularBooks",BookController.getParticularBooks )
+
+// 5.
+
+router.get("/getXINRBooks",BookController.getXINRBooks )
+
+// 6.
+
+router.get("/getRandomBooks",BookController.getRandomBooks  )
+
 
 module.exports = router;
